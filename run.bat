ea60749bee97871e26bda04f445f415a3e7701ac
lbsantos <lucasbrandaao1@gmail.com>
2025-02-19 11:14:02 -0300

    bat

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM Acessing jobconvo folder
+cd "SEU CAMINHO\GitHub\RPA_SALA_ATENDIMENTO"
+
+REM Run jobconvo script
+pipenv run python "SEU CAMINHO \GitHub\RPA_SALA_ATENDIMENTO\acessoSOC.py"
